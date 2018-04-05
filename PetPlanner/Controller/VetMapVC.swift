931f023362f1d93a04a2db06fdfb0f2ad952e7ba
Caroline Davis <caroline_davis@live.com>
2018-04-05 16:56:45 +1000

    feat: Start code for mapview

--- PetPlanner/Controller/VetMapVC.swift
+++ PetPlanner/Controller/VetMapVC.swift
@@ -7,17 +7,68 @@
 //
 
 import UIKit
+import MapKit
 
-class VetMapVC: UIViewController {
 
+class VetMapVC: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    private var locationManager: CLLocationManager!
+    private var currentLocation: CLLocation?
+  
+    let regionRadius: CLLocationDistance = 100
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        mapView.delegate = self
+
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+        
     }
-
-
     
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        defer { currentLocation = locations.last }
+        
+        if currentLocation == nil {
+            // Zoom to user location
+            if let userLocation = locations.last {
+                let viewRegion = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 1000, 1000)
+                mapView.setRegion(viewRegion, animated: true)
+            }
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        return pinView
+        
+    }
+  
 
 
 }
+
+
+
