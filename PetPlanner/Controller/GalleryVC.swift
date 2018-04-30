562a1b1bc4092dc917040cb0e8e43cc65a5c2964
Caroline Davis <caroline_davis@live.com>
2018-04-30 16:54:39 +1000

    feat: Start code for gallery vc

--- PetPlanner/Controller/GalleryVC.swift
+++ PetPlanner/Controller/GalleryVC.swift
@@ -7,13 +7,56 @@
 //
 
 import UIKit
+import SDWebImage
 
-class GalleryVC: UIViewController {
+class GalleryVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+   
+    
+    @IBOutlet weak var collection: UICollectionView!
+    
+    var collectionViewData = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collection.delegate = self
+        collection.dataSource = self
 
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        self.collection.reloadData()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return collectionViewData.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as? PhotoCell {
+            let petPhoto = self.collectionViewData[indexPath.row]
+            
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+         let petPhoto = self.collectionViewData[indexPath.row]
+        // get the photoId and save it so we can open it in the next window
+        let vc = storyboard?.instantiateViewController(withIdentifier: "ViewPhotoVC") as! ViewPhotoVC
+   //     vc.photoId = photoId
+        self.navigationController?.pushViewController(vc, animated: false)
+
+        
+    }
+    
     
 
    // Get profile picture and put it in as first gallery photo
---
+++ PetPlanner/View/PhotoCell.swift
@@ -0,0 +1,23 @@
+//
+//  PhotoCell.swift
+//  PetPlanner
+//
+//  Created by Caroline Davis on 30/4/18.
+//  Copyright © 2018 Caroline Davis. All rights reserved.
+//
+
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photo: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        photo.contentMode = .scaleAspectFit
+        
+        
+    }
+}
