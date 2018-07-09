c06ec254be9631305c755c6a27ac97c160bf3fff
Caroline Davis <caroline_davis@live.com>
2018-07-09 17:05:48 +1000

    feat: Fix border size of circular imgs

--- PetPlanner/View/CircularImgView.swift
+++ PetPlanner/View/CircularImgView.swift
@@ -14,19 +14,29 @@ class CircularImgView: UIImageView {
         super.awakeFromNib()
         
             colourSwap()
+            borderWidth()
             layer.cornerRadius = self.frame.width / 2
-            layer.borderWidth = 6
+          //  layer.borderWidth = 6
             layer.masksToBounds = true
 
     }
     
     func colourSwap() {
         if (self.viewWithTag(1) != nil) {
-            layer.borderColor = WHITE_COLOR.cgColor
-        } else {
             layer.borderColor = PINK_COLOR.cgColor
+        } else {
+            layer.borderColor = WHITE_COLOR.cgColor
         }
     }
+    
+    func borderWidth() {
+        if (self.viewWithTag(3) != nil) {
+            layer.borderWidth = 3
+        } else {
+            layer.borderWidth = 6
+        }
+    }
+ 
     
 
 }
