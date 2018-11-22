22e0d2ea4c432e2e2499aee2b0d7a9dabc0eb95c
Caroline Davis <caroline_davis@live.com>
2018-11-22 15:27:21 +1100

    fix: Changed the way view and add pets work with buttons rather than uialerts

--- PetPlanner/Controller/ViewPetsVC.swift
+++ PetPlanner/Controller/ViewPetsVC.swift
@@ -12,11 +12,12 @@ import SDWebImage
 
 class ViewPetsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var addPet: ImgAndTxtBtn!
+    @IBOutlet weak var paidVersion: ImgAndTxtBtn!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
-    var counter = 0
-    
+    var paid = false
     
     var tableViewData = [PetProfile]()
     
@@ -29,12 +30,20 @@ class ViewPetsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // need to do a check - prob a call to apple to see if user has paid or not to set the paidversion bool to true or false
+        
         tableView.delegate = self
         tableView.dataSource = self
         
         self.tableView.rowHeight = 100
         
+        addPet.isHidden = true
+        addPet.isEnabled = false
+        paidVersion.isHidden = true
+        paidVersion.isEnabled = false
+        
     }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(false)
@@ -43,32 +52,37 @@ class ViewPetsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         self.activityIndicator.startAnimating()
         
         DataService.ds.getAllPets { (pets) in
-            if pets == nil && self.counter == 0 {
-                self.noPets()
-            } else if self.counter != 0 {
-                self.navigationController?.popViewController(animated: true)
-            } else {
+            if pets != nil && self.paid == true {
+                self.addPet.isHidden = true
+                self.addPet.isEnabled = false
+                self.paidVersion.isHidden = true
+                self.paidVersion.isEnabled = false
                 self.tableViewData = pets!
                 self.tableView.reloadData()
-                self.activityIndicator.isHidden = true
-                self.activityIndicator.stopAnimating()
+            } else if pets == nil && self.paid == true {
+                self.addPet.isHidden = false
+                self.addPet.isEnabled = true
+                self.paidVersion.isHidden = true
+                self.paidVersion.isEnabled = false
+            } else if pets != nil && self.paid == false {
+                self.addPet.isHidden = true
+                self.addPet.isEnabled = false
+                self.paidVersion.isHidden = false
+                self.paidVersion.isEnabled = true
+                self.tableViewData = pets!
+                self.tableView.reloadData()
+            } else {
+                self.addPet.isHidden = false
+                self.addPet.isEnabled = true
+                self.paidVersion.isHidden = false
+                self.paidVersion.isEnabled = true
             }
+            self.activityIndicator.isHidden = true
+            self.activityIndicator.stopAnimating()
+            
         }
     }
     
-    func noPets() {
-        
-        counter += 1
-        let alert = UIAlertController(title: "Error", message: "You have no pets! Please add a pet", preferredStyle: UIAlertController.Style.alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
-            let vc = self.storyboard?.instantiateViewController(withIdentifier: "CreatePetVC") as! CreatePetVC
-            self.navigationController?.pushViewController(vc, animated: true)
-        }))
-        self.present(alert, animated: true, completion: nil)
-        
-        self.activityIndicator.isHidden = true
-        self.activityIndicator.stopAnimating()
-    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tableViewData.count 
@@ -119,7 +133,8 @@ class ViewPetsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             
             // if after deletion there are 0 pets, call the no pets func and segue to create a pet
             if self.tableViewData.count == 0 {
-                self.noPets()
+                self.addPet.isHidden = false
+                self.addPet.isEnabled = true
             }
             
             // removes the data from firebase
